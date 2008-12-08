a4c4d096083aa32b705d53179c4b1409d096134c
mjanous <m50mnj1@wpo.cso.niu.edu>
2008-12-08 08:30:27 -0600

    Added related_name to the semester and sponsor FKs of Project model.

--- models.py
+++ models.py
@@ -187,8 +187,8 @@ class Semester(models.Model):
     
 class Project(models.Model):
     project_name = models.CharField(max_length=60)
-    semester = models.ForeignKey(Semester)
-    sponsor = models.ForeignKey('Sponsor')
+    semester = models.ForeignKey(Semester, related_name='project_set')
+    sponsor = models.ForeignKey('Sponsor', related_name='project_set')
     sponsor_contacts = models.ManyToManyField('SponsorContact', related_name='projects', blank=True)
     
     # Managers
