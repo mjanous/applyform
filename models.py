6d9262c70166e74fa2596668b3fa9bad7e5488a2
mjanous <m50mnj1@wpo.cso.niu.edu>
2009-05-20 10:34:58 -0500

    project_purpose field can now be blank.

--- models.py
+++ models.py
@@ -235,7 +235,7 @@ class Semester(models.Model):
     
 class Project(models.Model):
     project_name = models.CharField(max_length=60)
-    project_purpose = models.TextField()
+    project_purpose = models.TextField(blank=True)
     semester = models.ForeignKey(Semester, related_name='project_set', blank=True, null=True)
     sponsors = models.ManyToManyField('Sponsor', related_name='project_set')
     sponsor_contacts = models.ManyToManyField('SponsorContact', related_name='projects', blank=True)
